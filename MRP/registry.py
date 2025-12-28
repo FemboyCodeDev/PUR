33a47b45ab4c024b56005cbc543ef385c4a4a2e6
Lexi <lexi@spifftech.uk>
2025-12-28 16:48:24 +0000

    Added command chains

--- MRP/registry.py
+++ MRP/registry.py
@@ -1,7 +1,7 @@
 
 
 
-class dataObject:
+class dataObject: # The actual data in the dataset
 	def __init__(self,type = "None"):
 		self.type = type # What type is this object, stored in a string
 		self.canHaveData = False # Can this object store other objects
@@ -21,6 +21,10 @@ class dataObject:
 			return "Not Applicable"
 		if self.type == "string":
 			return "".join([str(x) for x in self.rawData])
+		if self.type == "commandChain":
+			return ";".join([x._getDataString() for x in self.data])
+		if self.type == "command":
+			return str(self.rawData[0])
 
 class data:
 	def __init__(self,name = "", object = dataObject(type="None")):
@@ -31,10 +35,20 @@ class data:
 			self.object.addData(data)
 
 
+def createCommandChain(name = "chain", commands = []): # This defines a command chain, this can be used to execute complex code sequences
+	objects = []
+	for i, command in enumerate(commands):
+		obj = dataObject(type = "command")
+		obj.rawData = [ command]
+		objects.append(obj)
+	obj = dataObject(type = "commandChain")
+	obj.data = objects
+	return data(name = name,object = obj)
 
 class dataset:
-	def __init__(self):
+	def __init__(self, name = "Unnamed Dataset"):
 		self.data = []
+		self.name = name
 	def addData(self,data):
 		self.data.append(data)
 
@@ -43,6 +57,7 @@ class dataset:
 
 
 def print_dataset(dataset):
+	print(f"Showing Dataset: {dataset.name}")
 	for data in dataset.data:
 		name = data.name
 		data = data.object.getData(format = "string")
--- MRP/translation.py
+++ MRP/translation.py
@@ -1,1 +1,9 @@
 import registry
+
+
+
+commands = registry.dataset()
+#registry.print_dataset(commands)
+
+commands.addData(registry.createCommandChain(name = "echo %x",commands = ["echo {x}"]))
+registry.print_dataset(commands)
