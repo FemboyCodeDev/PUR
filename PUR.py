98c9182a2a9741674a36939255f81088071044fc
Lexi <lexi@spifftech.uk>
2025-12-29 15:44:46 +0000

    Made it so that to run a file you have to add it as an argument when running the code, but when you dont it sends you to an interpretor

--- PUR.py
+++ PUR.py
@@ -4,8 +4,7 @@
 
 import MRP # Imports the registry system
 import EEP
-
-
+import argparse
 
 EEP.run.commands = MRP.translation.commands
 EEP.run.variables = MRP.registry.dataset(name = "Variables")
@@ -15,10 +14,35 @@ EEP.run.dataClass = MRP.registry.data
 
 
 
+
 if __name__ == "__main__":
-	code = "echo 'testing 123'"
-	code = ""
-	with open("test.PUR") as file:
-		code = file.read()
-	EEP.run.run(code)
-	MRP.registry.print_dataset(EEP.run.variables)
+	parser = argparse.ArgumentParser(
+		description = "PUR: Programmable Utilility Resource"
+	)
+	parser.add_argument("filename",nargs = "?")
+
+	args = parser.parse_args()
+	print(args)
+	if args.filename:
+		code = "echo 'testing 123'"
+		code = ""
+		with open("test.PUR") as file:
+			code = file.read()
+		EEP.run.run(code)
+		MRP.registry.print_dataset(EEP.run.variables)
+	else:
+		EEP.run.running = True
+		#global EEP.run._lineIndex
+		code = []
+		while EEP.run.running:
+			if EEP.run._lineIndex >= EEP.run._lineIndex:
+				line = input(">>>")
+				code.append(line)
+			try:
+				line = code[EEP.run._lineIndex]
+				if len("".join(line.split(" "))) > 0:
+					EEP.run.runLine(line)
+				EEP.run._lineIndex +=1
+			except Exception as e:
+				print(e)
+				EEP.run._lineIndex = len(code)
