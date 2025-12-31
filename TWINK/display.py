ac21c36ef5b8692d30bd5b4c69e03a65c60ea008
Lexi <lexi@spifftech.uk>
2025-12-31 14:54:21 +0000

    Implemented Image loading

--- TWINK/display.py
+++ TWINK/display.py
@@ -20,8 +20,8 @@ class display:
 
 		self.fontWidth = 7
 		self.fontWidth = 8
-		self.height = 256
-		self.width = 256
+		self.height = 512
+		self.width = 512
 		self.graphics = []
 		self.scrollx = 0
 		self.scrolly = 0
@@ -33,9 +33,12 @@ class display:
 	def addChar(self,x = 0,y = 0,char = " ", color = (255,0,0)):
 		object = displayChar(x = x, y = y, char = char,color = color)
 		self.addDisplayChar(object)
-
+		self.addObjectToHash(x,y,object)
+	
+	def addObjectToHash(self,x,y,object):
+		#print(hash)
 		hash = self.calculateHash(x,y)
-
+		#print(hash)
 		if hash not in self.locationHashes:
 			self.locationHashes[hash] = []
 
@@ -57,11 +60,20 @@ class display:
 		print(content)
 	def getChar(self,x = 0,y = 0):
 		char = " "
+		currentDepth = None
 		hash = self.calculateHash(x,y)
-		for item in self.locationHashes[hash]:
-			if item.atXY(x,y,scroll = (self.scrollx,self.scrolly)):
-				char = item.getChar()
+		#print(hash)
+		if hash in self.locationHashes:
+			for item in self.locationHashes[hash]:
+				print(item)
+				if item.atXY(x,y,scroll = (self.scrollx,self.scrolly)):
+					if currentDepth == None:
+						currentDepth = item.depth - 1
+					if not item.depth < currentDepth:
+						char = item.getChar()
+						currentDepth = item.depth
 		return char
+
 
 
 
@@ -72,6 +84,7 @@ class displayChar:
 		self.depth = 0
 		self.char = char
 		self.color = color
+		self.controlId = "world"
 	def getChar(self):
 		r,g,b = self.color
 		return colorise(r,g,b) +self. char
@@ -86,13 +99,42 @@ class displayChar:
 		return False
 
 
+
+
+def loadImage(data,window):
+	objects = []
+	for row in data.split("\n"):
+		x,y,char,r,g,b = 0,0," ",255,0,0
+		row = row.split(":")
+		if len(row) > 0:
+			if len(row) > 2:
+				x,y,char = row[0],row[1],row[2]
+				#print(x,y,char)
+				if len(row) > 3:
+					r,g,b = row[3],row[3],row[3]
+					if len(row) > 5:
+						r,g,b = row[3],row[4],row[5]
+				#print(x,y,char,r,g,b)
+
+				object = displayChar(x=int(x),y=int(y),char=char,color=(int(r),int(g),int(b)))
+				object.depth = 10
+				window.addDisplayChar(object)
+				window.addObjectToHash(int(x),int(y),object)				
+
+
+
 FullBlock = "█"
 
 
 
 if __name__  == "__main__":
 	window = display()
-	for x in range(0,64,1):
-		for y in range(0, 64, 1):
-			window.addChar(x = x, y = y, char = FullBlock,color = (x*4,y*4,0))
+	for x in range(0,128,1):
+		for y in range(0, 128, 1):
+			pass
+			#window.addChar(x = x, y = y, char = FullBlock,color = (x*2,y*2,0))
+
+
+	with open("img.TWINK") as file:
+		loadImage(file.read(),window)
 	window.render()
