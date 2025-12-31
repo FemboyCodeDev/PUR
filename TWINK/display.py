810b905f785afb0a991f09694a0a58b01563f454
Lexi <lexi@spifftech.uk>
2025-12-31 14:16:42 +0000

    Display can now show colours

--- TWINK/display.py
+++ TWINK/display.py
@@ -1,10 +1,10 @@
-
+#
 
 
 def colorise(r,g,b):
-	return "\\e[31m"
+#	return "\\e[31m"
 
-	return f"\\e[38;2;{r};{g};{b}m"
+	return f"\x1b[38;2;{r};{g};{b}m"
 
 
 class display:
@@ -21,36 +21,39 @@ class display:
 		self.scrolly = 0
 	def addDisplayChar(self,object):
 		self.graphics.append(object)
-	def addChar(self,x = 0,y = 0,char = " "):
-		object = displayChar(x = x, y = y, char = char)
+	def addChar(self,x = 0,y = 0,char = " ", color = (255,0,0)):
+		object = displayChar(x = x, y = y, char = char,color = color)
 		self.addDisplayChar(object)
 	def render(self):
+		content = ""
 		for sy in range(0,int(self.height/self.fontHeight)):
 			row = ""
 			for sx in range(0,int(self.width/self.fontWidth)):
 				x = sx + self.scrollx
 				y = sy + self.scrolly
 				row += self.getChar(x=x,y=y)
-			print(row)
+			#print(row)
+			content += row+ "\n"
+		print(content)
 	def getChar(self,x = 0,y = 0):
 		char = " "
 		for item in self.graphics:
 			if item.atXY(x,y,scroll = (self.scrollx,self.scrolly)):
-				char = item.char
+				char = item.getChar()
 		return char
 
 
 
 class displayChar:
-	def __init__(self,x = 0,y = 0,char  = " "):
+	def __init__(self,x = 0,y = 0,char  = " ",color = (255,0,0)):
 		self.x = x
 		self.y = y
 		self.depth = 0
 		self.char = char
-		self.color = (255,0,0)
+		self.color = color
 	def getChar(self):
 		r,g,b = self.color
-		return colorise(r,g,b) + char
+		return colorise(r,g,b) +self. char
 	def getXY(self,scroll = (0,0)):
 		return self.x,self.y
 	def atXY(self,x,y,scroll = (0,0)):
@@ -68,7 +71,7 @@ FullBlock = "█"
 
 if __name__  == "__main__":
 	window = display()
-	for x in range(0,32,1):
-		for y in range(0, 32, 1):
-			window.addChar(x = x, y = y, char = FullBlock)
+	for x in range(0,64,1):
+		for y in range(0, 64, 1):
+			window.addChar(x = x, y = y, char = FullBlock,color = (x*4,y*4,0))
 	window.render()
